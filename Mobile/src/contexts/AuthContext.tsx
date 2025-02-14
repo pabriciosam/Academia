4169f5927421e9d057d4a877b119585ca35cfac9
Pabricio <pabricio.samagaio@gmail.com>
2025-02-14 09:31:51 -0300

    Criação de estrutura JWT para obter, armazenar, remover e validar o token do usuário.

--- Mobile/src/contexts/AuthContext.tsx
+++ Mobile/src/contexts/AuthContext.tsx
@@ -1,6 +1,7 @@
 import { createContext, ReactNode, useEffect, useState } from 'react'
 
 import { storageUserSave, storageUserGet, storageUserRemove } from '@storage/storeUser';
+import { storageAuthTokenGet, storageAuthTokenRemove, storageAuthTokenSave } from '@storage/storageAuthToken';
 
 import { UserDTO } from '@dtos/UserDTO';
 import { api } from '@services/api';
@@ -22,13 +23,36 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
   const [user, setUser] = useState({} as UserDTO);
   const [isLoadingUserStorageData, setIsLoadingUserStorageData] = useState(true);
 
+  async function userAndTokenUpdate(userData: UserDTO, token: string) {
+    api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+
+    setUser(userData);
+  }
+
+  async function storageUserAndTokenSave(userData: UserDTO, token: string){
+    try {
+      setIsLoadingUserStorageData(true);
+
+      await storageUserSave(userData);
+      await storageAuthTokenSave(token);  
+    } catch (error) {
+      throw error; 
+    }
+    finally{
+      setIsLoadingUserStorageData(false);
+    }
+  }
+
   async function singIn(email: string, password: string) {
     try {
       const {data} = await api.post('/sessions', { email, password });
 
-      if (data.user)
-        setUser(data.user);
-        storageUserSave(data.user);
+      if (data.user && data.token)
+      {
+          await storageUserAndTokenSave(data.user, data.token);
+
+          userAndTokenUpdate(data.user, data.token);
+      }
     }
     catch(error){
       throw error;
@@ -41,6 +65,7 @@ async function singOut(){
     setUser({} as UserDTO);
 
     await storageUserRemove();
+    await storageAuthTokenRemove();
   } catch (error) {
     throw error;
   }
@@ -51,11 +76,13 @@ async function singOut(){
 
   async function loadUserDate(){
     try {
-      const userLogged = await storageUserGet();
+      setIsLoadingUserStorageData(true);
 
-      if (userLogged){
-        setUser(userLogged);
-      }  
+      const userLogged = await storageUserGet();
+      const token = await storageAuthTokenGet();
+
+      if (token && userLogged)
+        userAndTokenUpdate(userLogged, token);
     } catch (error) {
       throw error;
     }
